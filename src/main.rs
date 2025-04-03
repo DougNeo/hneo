71f69dfb14a5c7e4336e1ba2de8155b48eeeffd9
Douglas Silva Santos <doug.neo@gmail.com>
2025-04-03 16:51:29 -0300

    Terminado a lição 2

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,32 @@ use sysinfo::{System, SystemExt, ProcessExt};
 fn main() {
     println!("Gerenciador de Processos!");
 
-    // Vamos inicializar o sistema e coletar informações.
+    // Inicializa o sistema e coleta informações
     let mut system = System::new_all();
     system.refresh_all();
 
-    println!("{:^8}|{:^15}|{:^10}", "PID", "NOME", "MEM (KB)");
+    // Cabeçalho com alinhamento consistente
+    println!("+-{:-<8}-+-{:-<15}-+-{:-<10}-+", "", "", "");
+    println!("| {:^8} | {:^15} | {:^10} |", "PID", "NOME", "MEM (KB)");
+    println!("+-{:-<8}-+-{:-<15}-+-{:-<10}-+", "", "", "");
 
-    for (pid, process) in system.processes()  {
-        println!("{:^8}|{:^15}|{:^10}", 
-                 pid, 
-                 process.name(), 
-                 process.memory() / 1024);
+    // Lista processos
+    for (pid, process) in system.processes() {
+        // Trunca nomes longos para manter o alinhamento
+        let name = if process.name().len() > 15 {
+            &process.name()[..15]
+        } else {
+            process.name()
+        };
+
+        // Formatação dos dados
+        println!(
+            "| {:^8} | {:^15} | {:^10} |", 
+            pid.to_string(),          // PID alinhado à direita
+            name,         // Nome alinhado à esquerda (com truncamento)
+            process.memory() / 1024 // Memória alinhada à direita
+        );
     }
-}
 
+    println!("+-{:-<8}-+-{:-<15}-+-{:-<10}-+", "", "", "");
+}
