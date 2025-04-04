2602270e8cd92cc8bb5421f57720d1909242c175
DougNeo <doug.neo@gmail.com>
2025-04-03 22:49:37 -0300

    adicionado nix ao projeto e agora estou pausando, continuado e matando processo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,80 @@
 use sysinfo::{System, SystemExt, ProcessExt};
+use nix::sys::signal::{kill, Signal};
+use nix::unistd::Pid;
+use std::error::Error;
 
 fn main() {
     println!("Gerenciador de Processos!");
+    
+    loop {
+        println!("\nEscolha uma opção:");
+        println!("0. Para Listar Processos");
+        println!("1. Pausar Processo");
+        println!("2. Continuar Processo");
+        println!("3. Matar processo");
+        println!("4. Sair");
 
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim() {
+            "0" => {
+                list_process();
+            }
+            "1" => {
+                let pid = get_pid_from_user();
+                if let Err(e) = pause_process(pid) {
+                    eprintln!("Erro ao pausar processo: {}", e);
+                }
+            }
+            "2" => {
+                let pid = get_pid_from_user();
+                if let Err(e) = continue_process(pid) {
+                    eprintln!("Erro ao continuar processo: {}", e);
+                }
+            }
+            "3" => {
+                let pid = get_pid_from_user();
+                if let Err(e) = kill_process(pid) {
+                    eprintln!("Erro ao finalizar processo: {}", e);
+                }
+            }
+            "4" => break,
+            _ => println!("Opção inválida!"),
+        }
+    }
+    println!("Até logo!");
+}
+
+fn get_pid_from_user() -> i32 {
+    println!("Digite o PID do processo:");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse::<i32>().unwrap_or(-1)
+}
+
+fn pause_process(pid: i32) -> Result<(), Box<dyn Error>> {
+    // Envia o sinal para pausar o processo
+    kill(Pid::from_raw(pid), Signal::SIGSTOP)?;
+    println!("Proceso {} pausado com suceso", pid);
+    Ok(())
+}
+
+fn continue_process(pid: i32) -> Result<(), Box<dyn Error>> {
+    // Envia sinal para continuar o processo
+    kill(Pid::from_raw(pid), Signal::SIGCONT)?;
+    println!("Processo {} retomado com sucesso", pid);
+    Ok(())
+}
+
+fn kill_process(pid: i32) -> Result<(), Box<dyn Error>> {
+    // Envia sinal para matar o processo
+    kill(Pid::from_raw(pid), Signal::SIGKILL)?;
+    println!("Processo {} encerrado com sucesso", pid);
+    Ok(())
+}
+
+fn list_process() {
     // Inicializa o sistema e coleta informações
     let mut system = System::new_all();
     system.refresh_all();
